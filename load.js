139e2453193c6205a973b7cca9f1292edf305191
IceSuger <icesugernowater@gmail.com>
2014-10-29 00:41:15 +0800

    to solve Oauth2 not defined

--- load.js
+++ load.js
@@ -1,19 +1,10 @@
-window.onload = function () {
-
-  var jq = document.createElement('script');
-  jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
-  document.getElementsByTagName('body')[0].appendChild(jq);
-	
-  var gmsrc = document.createElement('script');
-  gmsrc.src = "https://xmailchrome.appspot.com/gmail.js";
-  document.getElementsByTagName('body')[0].appendChild(gmsrc);
-/*	
+window.onload = function () {	
 	var sm = document.createElement('script');
-  sm.src = "chrome://extensions/ibmlbdjegjfkfhgpbmkenbeejhgngjmc/passDOM.js";
+  sm.src = "https://rawgit.com/IceSuger/Gmail_Plugin/master/passDOM.js";
   document.getElementsByTagName('body')[0].appendChild(sm);
-	
+
 	var sm = document.createElement('script');
-  sm.src = "test/test4.js";
+  sm.src = "https://rawgit.com/IceSuger/Gmail_Plugin/master/test/test4.js";
   document.getElementsByTagName('body')[0].appendChild(sm);
 /*
   window.addEventListener("message", function(event) {
