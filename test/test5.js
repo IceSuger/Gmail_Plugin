f894494c2e79a94e6e1b1e6579e15507ef43b43f
IceSuger <icesugernowater@gmail.com>
2014-12-11 17:46:36 +0800

    character size fixed

--- test/test5.js
+++ test/test5.js
@@ -6,20 +6,72 @@ var DRAFT_URL_prefix = 'https://www.googleapis.com/gmail/v1/users/me/drafts/';//
 var token = '';
 var sortingtable;
 var TINY={};
-var div_closed = 1;
-/*
-//-----------------
-if(div_closed == 0)
-			{
-				div.style.display = 'block';
-			}
-			else
-			{
-				div.style.display = 'none';
-			}
-//--------------------
-*/
 
+	
+//===================表格排序分页的script.js全部内容===
+{
+function T$(i){return document.getElementById(i)}
+
+TINY.table=function(){
+	function sorter(n){this.n=n; this.pagesize=10; this.paginate=0}
+	sorter.prototype.init=function(e,f){
+		var t=ge(e), i=0; this.e=e; this.l=t.r.length; t.a=[];
+		t.h=T$(e).getElementsByTagName('thead')[0].rows[0]; t.w=t.h.cells.length;
+		for(i;i<t.w;i++){
+			var c=t.h.cells[i];
+			if(c.className!='nosort'){
+				c.className=this.head; c.onclick=new Function(this.n+'.wk(this.cellIndex)')
+			}
+		}
+		for(i=0;i<this.l;i++){t.a[i]={}}
+		if(f!=null){var a=new Function(this.n+'.wk('+f+')'); a()}
+		if(this.paginate){this.g=1; this.pages()}
+	};
+	sorter.prototype.wk=function(y){
+		var t=ge(this.e), x=t.h.cells[y], i=0;
+		for(i;i<this.l;i++){
+      t.a[i].o=i; var v=t.r[i].cells[y]; t.r[i].style.display='';
+      while(v.hasChildNodes()){v=v.firstChild}
+      t.a[i].v=v.nodeValue?v.nodeValue:''
+    }
+		for(i=0;i<t.w;i++){var c=t.h.cells[i]; if(c.className!='nosort'){c.className=this.head}}
+		if(t.p==y){t.a.reverse(); x.className=t.d?this.asc:this.desc; t.d=t.d?0:1}
+		else{t.p=y; t.a.sort(cp); t.d=0; x.className=this.asc}
+		var n=document.createElement('tbody');
+		for(i=0;i<this.l;i++){
+			var r=t.r[t.a[i].o].cloneNode(true); n.appendChild(r);
+			r.className=i%2==0?this.even:this.odd; var cells=r.getElementsByTagName('td');
+			for(var z=0;z<t.w;z++){cells[z].className=y==z?i%2==0?this.evensel:this.oddsel:''}
+		}
+		t.replaceChild(n,t.b); if(this.paginate){this.size(this.pagesize)}
+	};
+	sorter.prototype.page=function(s){
+		var t=ge(this.e), i=0, l=s+parseInt(this.pagesize);
+		if(this.currentid&&this.limitid){T$(this.currentid).innerHTML=this.g}
+		for(i;i<this.l;i++){t.r[i].style.display=i>=s&&i<l?'':'none'}
+	};
+	sorter.prototype.move=function(d,m){
+		var s=d==1?(m?this.d:this.g+1):(m?1:this.g-1);
+		if(s<=this.d&&s>0){this.g=s; this.page((s-1)*this.pagesize)}
+	};
+	sorter.prototype.size=function(s){
+		this.pagesize=s; this.g=1; this.pages(); this.page(0);
+		if(this.currentid&&this.limitid){T$(this.limitid).innerHTML=this.d}
+	};
+	sorter.prototype.pages=function(){this.d=Math.ceil(this.l/this.pagesize)};
+	function ge(e){var t=T$(e); t.b=t.getElementsByTagName('tbody')[0]; t.r=t.b.rows; return t};
+	function cp(f,c){
+		var g,h; f=g=f.v.toLowerCase(), c=h=c.v.toLowerCase();
+		var i=parseFloat(f.replace(/(\$|\,)/g,'')), n=parseFloat(c.replace(/(\$|\,)/g,''));
+		if(!isNaN(i)&&!isNaN(n)){g=i,h=n}
+		i=Date.parse(f); n=Date.parse(c);
+		if(!isNaN(i)&&!isNaN(n)){g=i; h=n}
+		return g>h?1:(g<h?-1:0)
+	};
+	return{sorter:sorter}
+}();
+
+}
 
 
 //===================表格排序相关代码==========================
@@ -28,12 +80,10 @@ var fileref=document.createElement("link");
 	fileref.href = "https://rawgit.com/IceSuger/Gmail_Plugin/master/style.css";
 document.getElementsByTagName("head")[0].appendChild(fileref);
 
-	
+
 
 //初始化表格着
 function InitDiv(){
-	var sorter = new TINY.table.sorter("sorter");
-	
 	var div = document.createElement('div');
 			div.id = "GmailAssist";
 			document.getElementsByTagName('body')[0].appendChild(div);
@@ -62,11 +112,9 @@ function InitDiv(){
 			sortingtable.border="0";
 			sortingtable.className="sortable";
 			
-			controls = document.createElement('div');
-			controls.id = 'controls';
+			
 			
 			div.appendChild(sortingtable);
-			div.appendChild(controls);
 			
 				{//table内容
 				var thead = document.createElement('thead');
@@ -118,7 +166,46 @@ function InitDiv(){
 				tbody.id='AttachmentsTableTbody2';
 				}
 			
-				{//controls内容
+	
+	
+				
+				
+	
+	
+
+}
+
+	
+
+//===================显示UI大块=================================
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.token != '')
+		{
+			token = request.token;
+			alert(token);
+			document.getElementById('GmailAssist').style.visibility = "visible";
+			fetchList();
+			
+					//-----初始化分页、排序
+	var sorter = new TINY.table.sorter("sorter");
+	sorter.head = "head";
+	sorter.asc = "asc";
+	sorter.desc = "desc";
+	sorter.even = "evenrow";
+	sorter.odd = "oddrow";
+	sorter.evensel = "evenselected";
+	sorter.oddsel = "oddselected";
+	sorter.paginate = true;
+	sorter.currentid = "currentpage";
+	sorter.limitid = "pagelimit";
+	sorter.init("table_to_sort",1);
+	
+	var controls = document.createElement('div');
+			controls.id = 'controls';
+			document.getElementById('GmailAssist').appendChild(controls);
+			
+	{//controls内容
 					var perpage = document.createElement('div');
 					controls.appendChild(perpage);
 					{
@@ -201,20 +288,7 @@ function InitDiv(){
 						text.appendChild(node);
 					}
 				}
-}
-
-InitDiv();	
-
-//===================显示UI大块=================================
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.token != '')
-		{
-			token = request.token;
-			alert(token);
-			document.getElementById('GmailAssist').style.visibility = "visible";
-			fetchList();
-			
+	
       sendResponse({farewell: "goodbye"});
 		}
   });
@@ -353,84 +427,6 @@ function fetchList() {
 
     xhr.send(null);
   }
+
 	
-//===================表格排序分页的script.js全部内容===
-{
-
-
-function T$(i){return document.getElementById(i)}
-function T$$(e,p){return p.getElementsByTagName(e)}
-
-TINY.table=function(){
-	function sorter(n){this.n=n; this.pagesize=10; this.paginate=0}
-	sorter.prototype.init=function(e,f){
-		var t=ge(e), i=0; this.e=e; this.l=t.r.length; t.a=[];
-		t.h=T$$('thead',T$(e))[0].rows[0]; t.w=t.h.cells.length;
-		for(i;i<t.w;i++){
-			var c=t.h.cells[i];
-			if(c.className!='nosort'){
-				c.className=this.head; c.onclick=new Function(this.n+'.wk(this.cellIndex)')
-			}
-		}
-		for(i=0;i<this.l;i++){t.a[i]={}}
-		if(f!=null){var a=new Function(this.n+'.wk('+f+')'); a()}
-		if(this.paginate){this.g=1; this.pages()}
-	};
-	sorter.prototype.wk=function(y){
-		var t=ge(this.e), x=t.h.cells[y], i=0;
-		for(i;i<this.l;i++){
-      t.a[i].o=i; var v=t.r[i].cells[y]; t.r[i].style.display='';
-      while(v.hasChildNodes()){v=v.firstChild}
-      t.a[i].v=v.nodeValue?v.nodeValue:''
-    }
-		for(i=0;i<t.w;i++){var c=t.h.cells[i]; if(c.className!='nosort'){c.className=this.head}}
-		if(t.p==y){t.a.reverse(); x.className=t.d?this.asc:this.desc; t.d=t.d?0:1}
-		else{t.p=y; t.a.sort(cp); t.d=0; x.className=this.asc}
-		var n=document.createElement('tbody');
-		for(i=0;i<this.l;i++){
-			var r=t.r[t.a[i].o].cloneNode(true); n.appendChild(r);
-			r.className=i%2==0?this.even:this.odd; var cells=T$$('td',r);
-			for(var z=0;z<t.w;z++){cells[z].className=y==z?i%2==0?this.evensel:this.oddsel:''}
-		}
-		t.replaceChild(n,t.b); if(this.paginate){this.size(this.pagesize)}
-	};
-	sorter.prototype.page=function(s){
-		var t=ge(this.e), i=0, l=s+parseInt(this.pagesize);
-		if(this.currentid&&this.limitid){T$(this.currentid).innerHTML=this.g}
-		for(i;i<this.l;i++){t.r[i].style.display=i>=s&&i<l?'':'none'}
-	};
-	sorter.prototype.move=function(d,m){
-		var s=d==1?(m?this.d:this.g+1):(m?1:this.g-1);
-		if(s<=this.d&&s>0){this.g=s; this.page((s-1)*this.pagesize)}
-	};
-	sorter.prototype.size=function(s){
-		this.pagesize=s; this.g=1; this.pages(); this.page(0);
-		if(this.currentid&&this.limitid){T$(this.limitid).innerHTML=this.d}
-	};
-	sorter.prototype.pages=function(){this.d=Math.ceil(this.l/this.pagesize)};
-	function ge(e){var t=T$(e); t.b=T$$('tbody',t)[0]; t.r=t.b.rows; return t};
-	function cp(f,c){
-		var g,h; f=g=f.v.toLowerCase(), c=h=c.v.toLowerCase();
-		var i=parseFloat(f.replace(/(\$|\,)/g,'')), n=parseFloat(c.replace(/(\$|\,)/g,''));
-		if(!isNaN(i)&&!isNaN(n)){g=i,h=n}
-		i=Date.parse(f); n=Date.parse(c);
-		if(!isNaN(i)&&!isNaN(n)){g=i; h=n}
-		return g>h?1:(g<h?-1:0)
-	};
-	return{sorter:sorter}
-}();
-}
-
-//-----初始化分页、排序
-
-	sorter.head = "head";
-	sorter.asc = "asc";
-	sorter.desc = "desc";
-	sorter.even = "evenrow";
-	sorter.odd = "oddrow";
-	sorter.evensel = "evenselected";
-	sorter.oddsel = "oddselected";
-	sorter.paginate = true;
-	sorter.currentid = "currentpage";
-	sorter.limitid = "pagelimit";
-	sorter.init("table_to_sort",1);
+InitDiv();
